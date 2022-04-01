4f156e94abd159ddc8cf2b7f3d0731c6c643c316
Sarah Johnson <sarah@Sarahs-MacBook-Pro.local>
2022-04-01 16:37:31 -0700

    changes

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -15,6 +15,7 @@ Users can buy NFTs using a newly deployed project ERC20 token -- check with gera
 - buy
 
 Royalties should be configurable for NFT sellers. e.g. take 10% of each sale, increase to 20%
+- only if you are the original creator
 
 Unit tests written for the contracts
 Deployment script written and contracts deployed to a testnet.
@@ -36,6 +37,9 @@ contract NFTMarketplace {
     ERC721 public NFT;
     uint256 public salePrice;
     uint256 public expirationTime;
+    uint256 public tokenId;
+    address public owner;
+
 
     struct NFTs {
         ERC721 NFT;
@@ -43,14 +47,28 @@ contract NFTMarketplace {
         uint256 expirationTime;
     }
 
-    NFTs[] public nfts;
+    NFTs[] private nfts;
+
+    function checkApproval(ERC721 _NFT) public returns (address) {
+        // Check approval for use with this contract
+        owner = _NFT.ownerOf(tokenId);
+        return owner;
+    }
 
     function addNFT(ERC721 _NFT, uint256 _salePrice, uint256 _expirationTime) public {
-        // Check approval for use with this contract
+
+        // List NFT on marketplace
         nfts.push(NFTs(_NFT, _salePrice, _expirationTime));
     }
 
     function getNFTMarketplace() public view returns (NFTs[] memory) {
         return nfts;
     }
+
+    function makeOffer() public {}
+
+    function buyNFT() public {}
+
+    function configureRoyalty() public {}
+
 }
