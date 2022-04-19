76eb582c988900fb9be8e75c1a59c4cc13b3b27b
Sarah Johnson <sarah@Sarahs-MacBook-Pro.local>
2022-04-19 10:46:30 -0500

    deploy script done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
-
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+let secret = require("./secret.json");
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    kovan: {
+      url: secret.url,
+      accounts: [secret.key]
     }
   }
 };
